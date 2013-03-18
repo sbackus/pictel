237cc47c42fec0b5c266de8665b597b5b26059ad
Michael Bernstein <bernstein.m.j@gmail.com>
2013-03-18 13:11:27 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pictel::Application.config.secret_token = '227a9125a25fec29489da7265e2aadf1e5c8efc340ba7f73727957e8c19486f38846c73ec74d75f081d31339ee8cb6e5d081c3d5e6e85a98a9f71171b3672dbb'

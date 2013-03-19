f183cfb7fd40898e9b77c694423b1ec2cfed0d14
Michael Bernstein <bernstein.m.j@gmail.com>
2013-03-19 12:09:30 -0400

    Relations between cards and stacks

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,6 @@
 class Card < ActiveRecord::Base
   attr_accessible :author, :current, :dataPicture, :dataText, :isText
+
+  #relates the card to the stack
+  belongs_to :stack
 end
--- app/models/stack.rb
+++ app/models/stack.rb
@@ -1,3 +1,6 @@
 class Stack < ActiveRecord::Base
   attr_accessible :active, :finished, :max_size
+
+  #relate the cards to the stack
+  has_many :cards
 end
